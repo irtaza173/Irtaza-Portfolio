1c601846d4527f26dc0f2061123d7bd613e23238
irtaz <irtaz@IRTAZA>
2026-06-11 12:08:11 +0500

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,6 +30,7 @@ export default function About() {
             </div>
           </motion.div>
 
+
           <motion.div
             initial={{ opacity: 0, x: 30 }}
             whileInView={{ opacity: 1, x: 0 }}
